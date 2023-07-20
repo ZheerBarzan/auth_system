1382a447a1ea84ce4bdce8c3c07c20d98bbde0ef
ZheerBarzan <zheerbarzan85@outlook.com>
2023-07-21 00:05:19 +0300

    added a signout feature to the code so that the user can log out

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("logged in")),
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: const Icon(
+              Icons.logout,
+            ),
+          ),
+        ],
+      ),
+      body: const Center(child: Text("logged in")),
     );
   }
 }
